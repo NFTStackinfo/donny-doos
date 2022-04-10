163c879e9eb54b3e3104931686043745d9cf251d
Hrach Hambardzumyan <hrach.hambardzumyan23@gmail.com>
2022-04-11 01:24:49 +0400

    collection section fix

--- src/components/ui/Carousel/CollectionCarousel.js
+++ src/components/ui/Carousel/CollectionCarousel.js
@@ -1,56 +1,60 @@
 import React, {useState} from 'react';
 import Slider from "react-slick";
-import Icon from "../icons/Icon";
 import collectionImagesData from "../../../data/collection-images-data";
 import {ArrowNext, ArrowPrev} from "../icons";
 
-const Arrow = ({ className, onClick, direction }) => {
-    return (
-        <button onClick={onClick} className={className}>
-            {direction === 'left' ? <ArrowPrev /> : <ArrowNext />}
-        </button>
-    )
+const Arrow = ({className, onClick, direction}) => {
+  return (
+    <button onClick={onClick} className={className}>
+      {direction === 'left' ? <ArrowPrev/> : <ArrowNext/>}
+    </button>
+  )
 }
 
 const CollectionCarousel = () => {
-    const [current, setCurrent] = useState(1)
-    const settings = {
-        infinite: true,
-        arrows: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        speed: 600,
-        centerMode: true,
-        centerPadding: '80px',
-        focusOnSelect: true,
-        variableWidth: true,
-        nextArrow: <Arrow direction={"right"}  />,
-        prevArrow: <Arrow direction={"left"}  />,
-        autoplay: true,
-        autoplaySpeed: 2000
-    };
+  const [current, setCurrent] = useState(1)
+  const settings = {
+    infinite: true,
+    arrows: true,
+    // rows: 3,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 600,
+    centerMode: true,
+    centerPadding: '0',
+    focusOnSelect: true,
+    variableWidth: true,
+    nextArrow: <Arrow direction={"right"}/>,
+    prevArrow: <Arrow direction={"left"}/>,
+    autoplay: true,
+    autoplaySpeed: 2000
+  };
 
-    const handleBeforeChange = async (oldIndex, newIndex) => {
-        setCurrent(newIndex + 1)
-    }
-    return (
-        <div className='img-slider-wrapper'>
-            <Slider className='slider img-slider' {...settings} beforeChange={handleBeforeChange}>
-                {[...Array(8)].map((item, index) => {
-                    return (
-                        <div className="item" key={`carousel-${index}`}>
-                            <div className="img-thumbnail">
-                                <img src={collectionImagesData[index]} alt="Donny Doos" />
-                            </div>
-                        </div>
-                    )
-                })}
-            </Slider>
-            <div className="item-number">
-                {current}/8
+  const handleBeforeChange = async (oldIndex, newIndex) => {
+    setCurrent(newIndex + 1)
+  }
+
+  return (
+    <div className='img-slider-wrapper'>
+      <Slider className='slider img-slider' {...settings}
+              beforeChange={handleBeforeChange}>
+        {[...Array(8)].map((item, index) => {
+          return (
+            <div className="item" key={`carousel-${index}`}>
+              <div className="item__container">
+                <div className="img-thumbnail">
+                  <img src={collectionImagesData[index]} alt="Donny Doos"/>
+                </div>
+              </div>
             </div>
-        </div>
-    );
+          )
+        })}
+      </Slider>
+      <div className="item-number">
+        {current}/8
+      </div>
+    </div>
+  );
 };
 
 export default CollectionCarousel;
