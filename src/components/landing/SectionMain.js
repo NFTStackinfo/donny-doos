15cbb5835e7500648f9e36e9f54e7289bae81817
AshotSimonyan <a.simonyan.stdev@gmail.com>
2022-04-15 18:12:22 +0400

    added video mute button

--- src/components/landing/SectionMain.js
+++ src/components/landing/SectionMain.js
@@ -1,15 +1,31 @@
-import React, {forwardRef} from 'react';
-import { isSafari } from 'react-device-detect'
+import React, { forwardRef } from 'react'
 
 const SectionMain = forwardRef((props, ref) => {
+  const [muted, setMuted] = React.useState('mute')
+  const customRef = React.useRef(null)
+
+  const toggleMute = () => {
+    let mute = customRef.current.muted
+
+    if (mute) {
+      customRef.current.muted = false
+      setMuted('unmute')
+    } else {
+      setMuted('mute')
+      customRef.current.muted = true
+    }
+  }
 
   return (
     <main className="section-main" id="main" ref={ref}>
-      <video muted={!isSafari} autoPlay playsInline loop className="section-main__banner">
+      <video muted autoPlay  playsInline loop className="section-main__banner" ref={customRef}>
         <source src="/assets/banner.webm" type="video/webm"/>
-        <source src="/assets/banner.mp4" type="video/mp4"/>
+        <source  src="/assets/banner.mp4" type="video/mp4"/>
         Your browser doesn't support HTML5 video tag.
       </video>
+      <div className='container'>
+        <button className='section-main__mute' onClick={toggleMute}><img src={`/assets/${muted}.png`} alt='' /></button>
+      </div>
     </main>
   );
 })
