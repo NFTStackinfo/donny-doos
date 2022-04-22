4cc22cde61c3476846060ad0cf6228b4e69d8215
Hrach Hambardzumyan <hrach.hambardzumyan23@gmail.com>
2022-04-22 12:44:04 +0400

    header-update-1. roadmap update-1.

--- src/data/roadmap-data.js
+++ src/data/roadmap-data.js
@@ -1,46 +1,47 @@
 export default {
-    phase1: [
-        {
-            title: '01/ Welcome to the Donny Doo’s',
-            description: 'Introduction to our story, team and the vision of the project will be done through our website, social media platforms and will follow up with regular AMAs.'
-        },
-        {
-            title: '02/ Launch',
-            description: 'Once we have committed community ready to make the real world and the metaverse a better place, mint date and exact supply amount will be announced.'
-        },
-        {
-            title: '03/ Mint & Contribution',
-            description: 'Portion of the proceeds will be allocated directly towards children in need – wordwide. With full transparency and as a DAO (Decentralized Autonomous Organization) - holders only will decide the organization receiving the donation.'
-        },
-        {
-            title: '04/ Giving back to the community',
-            description: 'We will open the podium to your projects in need of funding to diversify your initial investment and support the right ideas . We will be asking for royalties in return. Those will be stored in Donny Doo’s treasury wallet and re-distributed back to the community or to the next project.'
-        },
-        {
-            title: '05/ Donny Doo’s podcast',
-            description: 'Exclusive content for holders only will be produced in the form of podcasts and AMAs with experts within the space (NFT influencers, artists, developers, crypto experts and traders)'
-        },
-        {
-            title: '06/ Growth system',
-            description: 'After the launch, as the project grows and expands , we will be in need of new team members. All the hires will be prioritized within holders, only then the rest of the community.'
-        }
-    ],
-    phase2: [
-        {
-            title: '01/ Metaverse',
-            description: 'Land in the metaverse will be bought, where all the holders will gather and network. Professionals in coaching, mental and physical health will be hired to hold workshops and speeches for the holders.'
-        },
-        {
-            title: '02/ Donny Doo’s HQ',
-            description: 'Establishment of a physical Donny Doo’s headquarters will begin with amazing benefits for the community.'
-        },
-        {
-            title: '03/ Staking',
-            description: 'Staking will be introduced to our poject, with rewards being distributed in our very own digital currency.'
-        },
-        {
-            title: '04/ Donny Doo’s Philanthropy',
-            description: 'A unique system of monthly contributions through our project will be introduced.'
-        },
-    ]
+  phase1: [
+    {
+      title: '01/ Welcome to the Donny Doo’s',
+      description: 'Introduction to our story, team and the vision of the project will be done through our website, social media platforms and will follow up with regular AMAs.'
+    },
+    {
+      title: '02/ Launch',
+      description: 'Once we have committed community ready to make the real world and the metaverse a better place, mint date and exact supply amount will be announced.'
+    },
+    {
+      title: '03/ Mint & Contribution',
+      description: 'Portion of the proceeds will be allocated directly towards children in need – wordwide. With full transparency and as a DAO (Decentralized Autonomous Organization) - holders only will decide the organization receiving the donation.'
+    },
+    {
+      title: '04/ Giving back to the community',
+      description: 'We will open the podium to your projects in need of funding to diversify your initial investment and support the right ideas . We will be asking for royalties in return. Those will be stored in Donny Doo’s treasury wallet and re-distributed back to the community or to the next project.'
+    },
+    {
+      title: '05/ Donny Doo’s podcast',
+      description: 'Exclusive content for holders only will be produced in the form of podcasts and AMAs with experts within the space (NFT influencers, artists, developers, crypto experts and traders)'
+    },
+    {
+      title: '06/ Growth system',
+      description: 'After the launch, as the project grows and expands , we will be in need of new team members. All the hires will be prioritized within holders, only then the rest of the community.'
+    }
+  ],
+  phase2: [
+    {
+      title: '01/ Donny Doo’s Philanthropy',
+      description: 'A unique system of monthly contributions through our project will be introduced.'
+    },
+    {
+      title: '02/ Donny Doo’s HQ',
+      description: 'Establishment of a physical Donny Doo’s headquarters will begin with amazing benefits for the community.'
+    },
+    {
+      title: '03/ Staking',
+      description: 'Staking will be introduced to our poject, with rewards being distributed in our very own digital currency.'
+    },
+
+    {
+      title: '04/ Metaverse',
+      description: 'Land in the metaverse will be bought, where all the holders will gather and network. Professionals in coaching, mental and physical health will be hired to hold workshops and speeches for the holders.'
+    }
+  ]
 }
