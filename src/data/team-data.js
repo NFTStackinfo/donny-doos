1badd7496a6a7429821a593a07295a294dd883f7
Hrach Hambardzumyan <hrach.hambardzumyan23@gmail.com>
2022-05-05 23:28:47 +0400

    Team member data update.

--- src/data/team-data.js
+++ src/data/team-data.js
@@ -7,19 +7,17 @@ export default [
     {
         image: '/assets/donny-doo-5.png',
         name: 'Meru',
-        description:<p>Founder, well established businessman, creative mind and NFT/Crypto Enthusiast.</p>
+        description:<p>Founder, well established entrepreneur, creative mind and NFT/Crypto Enthusiast.</p>
     },
     {
         image: '/assets/donny-doo-7.png',
         name: 'Chris',
-        description:<p>Art supervisor, Artist <br/>
-            Multitalented artist, with years of experience and passion for NFTs.</p>
+        description:<p>Art supervisor, multitalented artist, with years of experience and passion for NFTs.</p>
     },
     {
         image: '/assets/donny-doo-6.png',
         name: 'Nick',
-        description:<p>Artist <br/>
-            Talented Lead artist of the project</p>
+        description:<p>Talented Lead artist of the project.</p>
     },
     {
         image: '/assets/donny-doo-2.png',
